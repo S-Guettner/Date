0af0860dd29044bf4353d98b057c4393cdde4759
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-09 12:57:20 +0100

    finish

---
+++ 1_1/main.js
@@ -0,0 +1,9 @@
+let date1 = new Date("September 2, 2019 09:00:00")
+let date2 = new Date(0)
+let date3 = new Date(31556908800)
+let date4 = new Date(86400000)
+
+const date1Output = document.querySelector("#date1").innerHTML = date1
+const date2Output = document.querySelector("#date2").innerHTML = date2
+const date3Output = document.querySelector("#date3").innerHTML = date3
+const date4Output = document.querySelector("#date4").innerHTML = date4
