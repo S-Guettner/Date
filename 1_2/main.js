71d104f4e633e390152c97206c11c168bf93f9d4
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-09 13:36:46 +0100

    output

--- 1_2/main.js
+++ 1_2/main.js
@@ -14,13 +14,13 @@ let monate = [
 ]
 
 let wochenTag = [
-    "Sonntag",
-    "Montag",
-    "Dienstag",
-    "Mittwoch",
-    "Donnerstag",
-    "Freitag",
-    "Samstag"
+    "Sun",
+    "Mon",
+    "Tues",
+    "Wed",
+    "Thurs",
+    "Fri",
+    "Sat"
 ]
 
 let date = new Date()
@@ -31,3 +31,15 @@ console.log(date.getHours())
 console.log(date.getMinutes())
 console.log(wochenTag[date.getDay()])
 console.log(monate[date.getMonth()])
+
+let minutesOutput = date.getMinutes()
+minutesOutput = minutesOutput <= 9 ? "0" + minutesOutput : minutesOutput
+let secondsOutput = date.getSeconds()
+secondsOutput = secondsOutput <= 9 ? "0" + secondsOutput : secondsOutput
+let dayTime = date.getHours() <= 12 ? "PM" : "AM"
+
+const day = document.querySelector("#day").innerHTML = wochenTag[date.getDay()]
+const hour = document.querySelector("#hour").innerHTML = date.getHours()
+const minutes = document.querySelector("#minutes").innerHTML = minutesOutput
+const seconds = document.querySelector("#seconds").innerHTML = secondsOutput
+const dayTimeOutput = document.querySelector("#dayTime").innerHTML = dayTime
