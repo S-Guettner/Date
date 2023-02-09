4ebab7a3ae9f36c6a262fd082372dc59c34879b1
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-09 14:25:33 +0100

    finish

--- 1_6/main.js
+++ 1_6/main.js
@@ -3,6 +3,7 @@ date2 = new Date()
 date3 = new Date(2019, 12, 3, 12)
 date4 = new Date(2000, 1, 1, 11)
 
+
 console.log(date1.getHours() <= 12 ? "PM" : "AM")
 console.log(date2.getHours() <= 12 ? "PM" : "AM")
 console.log(date3.getHours() <= 12 ? "PM" : "AM")
